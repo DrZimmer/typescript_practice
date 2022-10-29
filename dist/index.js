b3b0fde73f7d5f9af06dba726d0eca1fca9565e2
White Tiger <dayley.zimmermann@gmail.com>
2022-10-28 22:37:20 -0400

    just messing around with typescript, nothing to see here

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,6 @@
 "use strict";
 let age = 20;
+function ages() {
+    let age = "hi";
+    return age;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 let age: number = 20;
+//test
+function ages() {
+  let age: string = "hi";
+  return age;
+}
