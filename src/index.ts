df829ba062a22c1e25f4e913ca1c583e2a3dfe30
White Tiger <dayley.zimmermann@gmail.com>
2022-11-02 21:22:06 -0400

    working with basic typescript objects

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-function calculateTax(income: number): number {
-  if (income < 50_000) return income * 1.2;
-  return income * 1.3;
-}
+// function calculateTax(income: number): number {
+//   if (income < 50_000) return income * 1.2;
+//   return income * 1.3;
+// }
+let employee: {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+} = {
+  id: 1,
+  name: "",
+  retire: (date: Date) => {
+    console.log(date);
+  }
+};
